1b2408c1bd557d1d42a02a72c59fa33239bb2e5e
merlinoa <andy.merlino@tychobra.com>
2022-12-08 06:06:46 -0500

    cleanup

--- examples/proxy_server/proxy.js
+++ examples/proxy_server/proxy.js
@@ -1,21 +1,13 @@
 import express from 'express'
 import polishedProxy from '../../index.js'
 
-const proxyMiddleware = await polishedProxy({
-  //appDir: '../shiny_min',
-  appDir: '../shiny_w_polished',
-  maxSessions: 2
-})
-
-
 const app = express()
 
-app.get("/favicon.ico", (req, res) => {
-  res.status(404).json({message: "not found"})
+const proxyMiddleware = await polishedProxy({
+  appDir: '../shiny_w_polished',
+  maxSessions: 2
 })
 
 app.use(proxyMiddleware)
 
 app.listen(8080)
-
-//app.on('upgrade', ws_proxy.upgrade) 

8c122e44ed315256d3aee2b95999234298092026
merlinoa <andy.merlino@tychobra.com>
2022-12-22 09:01:27 -0500

    working on basic dashboard and organization

--- examples/plb_1/server.js
+++ examples/plb_1/server.js
@@ -1,35 +1,25 @@
 import express from 'express'
 import plb, { get_sessions_data } from '../../index.js'
-import htmlExpress from 'html-express-js'
 
 const app = express()
 
-const proxy = await polishedProxy({
+const proxy = await plb({
   appDir: '../shiny_w_polished',
   maxSessions: 2
 })
 
-// set up engine
-app.engine(
-  'js',
-  htmlExpress({
-    includesDir: 'includes', // where all includes reside
-  })
-);
-// use engine
-app.set('view engine', 'js');
 
-// set directory where all index.js pages are served
-app.set('views', './public')
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 
 app.get("/polished-proxy", (req, res) => {
   
   // TODO: could check that user is an admin here
 
-  const dat = get_sessions_data(proxy.shinyApp)
-  
-  res.render('dashboard', dat);
-  
+  //const dat = get_sessions_data(proxy.shinyApp)
+  //console.log("dat: ", dat)
+  res.render('dashboard')
   
 })
 
--- examples/plb_1/views/dashboard_old.js
+++ examples/plb_1/views/dashboard_old.js
@@ -20,15 +20,15 @@ const box = obj => html`<div style="background-color: F5F5F5; padding: 15px; bor
   </table>
 </div>`
 
-export const view = (totals, apps) => html`<html>
+export const view = (data, state) => html`<html>
   <head></head>
   <body>
     <h1>Polished Load Balancer</h1>
-    <h3>Active Sessions: ${totals.active}</h3>
-    <h3>Inactive Sessions: ${totals.inactive}</h3>
-    <h3>Total Sessions: ${totals.active + totals.inactive}</h3>
+    <h3>Active Sessions: ${data.totals.active}</h3>
+    <h3>Inactive Sessions: ${data.totals.inactive}</h3>
+    <h3>Total Sessions: ${data.totals.active + data.totals.inactive}</h3>
     <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 15px;">
-      ${apps.map(obj => box(obj))}
+      ${data.apps.map(obj => box(obj))}
     </div>
   </body>
 </html>`
